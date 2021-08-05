b11cbf2e54c79227273a5505675311e80a171f7c
Luis Mendez <dluismendezdj@gmail.com>
2021-08-05 14:13:22 -0400

    Delete Todo finished at views.py and urls.py (todo)

--- todo/views.py
+++ todo/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from .models import Todo
 
 def todo_list(request):
+    """ Return a list of task created """
     todo_data = Todo.objects.all()
 
     return render(request, 'todo/todo_list.html', {
@@ -13,7 +14,15 @@ def todo_list(request):
     })
 
 def insert_todo(request:HttpRequest):
-    todo = Todo(content = request.POST['task'])
-    todo.save()
+    """ Insert a new task """
+    todo_to_insert = Todo(content = request.POST['task'])
+    todo_to_insert.save()
+
+    return redirect('todo_list')
+
+def delete_todo(request, todo_id):
+    """ Delete a task """
+    todo_to_delete = Todo.objects.get(id = todo_id)
+    todo_to_delete.delete()
 
     return redirect('todo_list')

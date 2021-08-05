0ac626c8dfe64f0fcbade689b04df3eeaf420a78
Luis Mendez <dluismendezdj@gmail.com>
2021-08-05 14:21:39 -0400

    Adding static files-configuration and base.html

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -131,6 +131,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/img/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

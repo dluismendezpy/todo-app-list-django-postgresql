f9e2811ad9c9a949721e00cb99b5af70dc377690
Luis Mendez <dluismendezdj@gmail.com>
2021-08-05 13:30:04 -0400

    Configure urls.py at todo_project

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -1,9 +1,10 @@
 # Django
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 # Owns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('todo/', include('todo.urls'))
 ]
